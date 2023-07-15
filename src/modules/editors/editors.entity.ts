b0557f35e5418309746f96ff5d9a93c66e3ac42a
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-07-15 10:46:20 +0500

    feat: refactored editors module

--- src/modules/editors/editors.entity.ts
+++ src/modules/editors/editors.entity.ts
@@ -4,29 +4,29 @@ import {
   Column,
   ManyToOne,
   JoinColumn,
-} from "typeorm";
-import { Admin } from "../admin/admin.entity";
-import { News } from "../news/news.entity";
+} from 'typeorm';
+import { Admin } from '../admin/admin.entity';
+import { News } from '../news/news.entity';
 
-@Entity("news_editor")
+@Entity('news_editor')
 export class NewsEditor {
-  @PrimaryGeneratedColumn("uuid")
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @ManyToOne(() => Admin, (admin) => admin.editors, {
     cascade: true,
-    onDelete: "SET NULL",
+    onDelete: 'CASCADE',
   })
   @JoinColumn()
   editor: Admin;
 
   @ManyToOne(() => News, (news) => news.editors, {
     cascade: true,
-    onDelete: "CASCADE",
+    onDelete: 'CASCADE',
   })
   @JoinColumn()
   news: News;
 
-  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   editedDate: Date;
 }
--- src/modules/editors/editors.service.ts
+++ src/modules/editors/editors.service.ts
@@ -14,93 +14,94 @@ export class NewsEditorService {
   ) {}
 
   async getAll(): Promise<NewsEditor[]> {
-    try {
       const newsEdits = await this.newsEditorRepository.find();
       return newsEdits;
-    } catch (err) {
-      throw new HttpException(true, 500, err.message);
-    }
   }
 
   async getById(id: string): Promise<NewsEditor> {
-    try {
-      const category = await this.newsEditorRepository.findOne({
+      const edition = await this.newsEditorRepository.findOne({
         where: { id },
+        relations: {
+          editor: {
+            position: true,
+          },
+        },
       });
-      return category;
-    } catch (error) {
-      throw new HttpException(true, 500, error.message);
-    }
+      return edition;
+  }
+
+  async getByAdminAndNewsId(editorId: string, newsId: string) {
+    const edition = await this.newsEditorRepository.findOne({
+      where: {
+        editor: { id: editorId },
+        news: { id: newsId },
+      },
+      order: {
+        editedDate: "DESC",
+      },
+    });
+    return edition;
   }
 
   async getByNewsId(id: string): Promise<NewsEditor> {
-    try {
       const category = await this.newsEditorRepository.findOne({
         where: { news: { id } },
         order: {
-          editedDate: 'DESC',
+          editedDate: "DESC",
         },
       });
       return category;
-    } catch (error) {
-      throw new HttpException(true, 500, error.message);
-    }
   }
 
   async create(values: CreateEditorDto) {
-    try {
       const response = await this.newsEditorRepository
         .createQueryBuilder()
         .insert()
         .into(NewsEditor)
         .values(values as unknown as NewsEditor)
+        .returning("id")
         .execute();
       return response;
-    } catch (error) {
-      throw new HttpException(true, 500, error.message);
-    }
   }
 
-  async updateEditDate(id: string, date: Date) {
-    try {
-      const edition = await this.getById(id);
-      if (IsSameDate(date, edition.editedDate)) {
-        const response = await this.newsEditorRepository.update(id, {
-          editedDate: date,
-        });
-        return response;
-      } else {
+  async updateEditDate(newsId: string, editorId: string) {
+      const edition = await this.getByAdminAndNewsId(editorId, newsId);
+      const date = new Date();
+      if (!edition) {
         const response = await this.create({
-          editor: edition.editor.id,
-          news: edition.news.id,
+          editor: editorId,
+          news: newsId,
           editedDate: date,
         });
-        return response;
+        return response.raw[0].id;
+      } else {
+        if (IsSameDate(date, edition.editedDate)) {
+          const response = await this.newsEditorRepository.update(edition.id, {
+            editedDate: date,
+          });
+          return edition.id;
+        } else {
+          const response = await this.create({
+            editor: editorId,
+            news: newsId,
+            editedDate: date,
+          });
+          return response.raw[0].id;
+        }
       }
-    } catch (err) {
-      return new HttpException(true, 500, err.message);
-    }
   }
 
   async remove(id: string): Promise<DeleteResult | HttpException> {
-    try {
       const response = await this.newsEditorRepository.delete(id);
-      return new HttpException(false, 204, 'Edition removed!');
-    } catch (error) {
-      return new HttpException(true, 500, error.message);
-    }
+      return new HttpException(false, 204, "Edition removed!");
   }
 
   async removeMoreByIds(ids: string): Promise<DeleteResult | HttpException> {
-    try {
       const response = await this.newsEditorRepository
         .createQueryBuilder()
         .delete()
-        .where('id IN(:...ids)', { ids })
+        .where("id IN(:...ids)", { ids })
         .execute();
       return response;
-    } catch (error) {
-      throw new HttpException(true, 500, error.message);
-    }
   }
 }
